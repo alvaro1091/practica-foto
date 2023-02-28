4e1ac85865db1d08ded9a5b1e7592191e1c857b6
Alvaro Rodriguez <carlosml1091@gmail.com>
2023-02-28 17:35:45 -0400

    Inicio la practica con la actividad 1 y 2

---
+++ actividad1.py
@@ -0,0 +1,5 @@
+#Seccion basica para python
+#solicitar nombre y edad del usuario
+nombre= input("ingresa tu nombre: ")
+edad= input("Especifica tu edad: ")
+print("Hola "+nombre + ", tu edad es "+edad)
---
+++ actividad2.py
@@ -0,0 +1,5 @@
+# calcule el area de un triangulo dados la base y la altura
+base = float(input("Ingresa la base del triangulo: "))
+altura= float(input("Ingrese la altura: "))
+area = 0.5 * base * altura
+print("El area del triangulo es: ", area)
